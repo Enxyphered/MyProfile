43038c15034bf40a7014527c877a5da38a3fe69d
Shaamil Ahmed <shaamilahmed@gmail.com>
2025-08-03 18:31:10 +0500

    Change the build to support easy debugging configuration. Added Work in progress and updated font sizes in mobile view Created publish script to auto update files in gitpage

--- MyProfile.WebApp/Program.cs
+++ MyProfile.WebApp/Program.cs
@@ -12,7 +12,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://enxyphered.github.io/MyProfile/") });
+#if DEBUG
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+#else
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://enxyphered.github.io/MyProfile/") });
+#endif
+
 builder.Services
     .AddBlazorise(options =>
     {
